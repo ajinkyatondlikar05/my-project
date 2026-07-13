4b085aee9dc826c7f5b30c728bb4ca5aec7a59d1
ajinkyatondlikar05 <ajinkyatondlikar@gmail.com>
2026-07-13 09:40:39 +0530

    Second Commit

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main()
 {
-printf("Hello World! This is Geeksforgeeks.org ")
+printf("Hello World! This is GeeksforGeeks.org\n");
+printf("My Name is Ajinkya Tondlikar\n");
 return 0;
 }
 
